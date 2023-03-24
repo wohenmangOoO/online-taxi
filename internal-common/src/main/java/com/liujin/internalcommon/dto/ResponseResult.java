7c4ad72f6e02dbf3f14c4e5ce09188c4931f767e
liujin <liujin103827@163.com>
2023-03-24 17:15:49 +0800

    封装响应信息

---
+++ internal-common/src/main/java/com/liujin/internalcommon/dto/ResponseResult.java
@@ -0,0 +1,49 @@
+package com.liujin.internalcommon.dto;
+
+import com.liujin.internalcommon.constant.CommonStatusEnum;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true) //链式调用
+public class ResponseResult <T>{
+    private Integer code;
+    private String message;
+    private T data;
+
+    //成功
+    public static <T> ResponseResult success(T data){
+        return new ResponseResult()
+                .setCode(CommonStatusEnum.SUCCESS.getCode())
+                .setMessage(CommonStatusEnum.SUCCESS.getValue())
+                .setData(data);
+    }
+
+    /**
+     * 失败：统一失败
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public static <T> ResponseResult fail(T data){
+        return new ResponseResult().setData(data);
+    }
+    /**
+     * 失败：提示错误码和信息
+     * @param code
+     * @param massage
+     * @return
+     */
+    public static ResponseResult fail(int code, String massage){
+        return new ResponseResult()
+                .setCode(CommonStatusEnum.FAIL.getCode())
+                .setMessage(CommonStatusEnum.FAIL.getValue());
+    }
+
+    public static <T> ResponseResult fail(int code, String massage, T data){
+        return new ResponseResult()
+                .setCode(CommonStatusEnum.FAIL.getCode())
+                .setMessage(CommonStatusEnum.FAIL.getValue())
+                .setData(data);
+    }
+}
