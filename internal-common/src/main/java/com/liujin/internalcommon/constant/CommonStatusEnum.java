abe6f6fa900fbaafd8dddf83337b0dcfc5c59821
liujin <liujin103827@163.com>
2023-03-24 17:15:08 +0800

    枚举响应信息

--- internal-common/src/main/java/com/liujin/internalcommon/constant/CommonStatusEnum.java
+++ internal-common/src/main/java/com/liujin/internalcommon/constant/CommonStatusEnum.java
@@ -1,4 +1,27 @@
 package com.liujin.internalcommon.constant;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
 public enum CommonStatusEnum {
+
+    //成功 CommonStatusEnum SUCCESS = new CommonStatusEnum();
+    SUCCESS(1,"success"),
+    FAIL(0,"fail");
+
+    private int code;
+    private String value;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    CommonStatusEnum(int code, String value) {
+        this.code = code;
+        this.value = value;
+    }
 }
