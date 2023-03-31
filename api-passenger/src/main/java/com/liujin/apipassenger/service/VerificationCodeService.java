2643ce613cb2621333e36d65723cc9214556b217
liujin <liujin103827@163.com>
2023-03-31 23:11:26 +0800

    乘客获取验证码

--- api-passenger/src/main/java/com/liujin/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/liujin/apipassenger/service/VerificationCodeService.java
@@ -1,21 +1,39 @@
 package com.liujin.apipassenger.service;
 
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import jdk.nashorn.internal.runtime.JSONFunctions;
+import com.liujin.apipassenger.remote.ServiceVerificationCodeClient;
+import com.liujin.internalcommon.dto.ResponseResult;
+import com.liujin.internalcommon.response.NumberCodeResponse;
 import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class VerificationCodeService {
 
-    public String getPassengerPhone(String passengerphone ){
-        System.out.println("获取验证码服务");
-        System.out.println("放入到redis" );
-        JSONObject jsonObject = new JSONObject();
-        String code = "1111";
-        jsonObject.put("code",1);
-        jsonObject.put("message","sucess");
-        return jsonObject.toString();
+    @Autowired
+    private ServiceVerificationCodeClient serviceVerificationCodeClient;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    private String verificationCodePrefix = "passenger-verification-code-";
+
+    public ResponseResult getPassengerPhone(String passengerphone ){
+        int numbercode = numberCoderesponse();
+        setRedis(passengerphone,numbercode);
+        return ResponseResult.success();
+    }
+    private int numberCoderesponse(){
+        ResponseResult<NumberCodeResponse> numberCoderesponse = serviceVerificationCodeClient.getNumberCode(6);
+        int numberCode = numberCoderesponse.getData().getNumbercode();
+        return numberCode;
+    }
+    private void setRedis(String passengerphone, int numberCode){
+        String key = verificationCodePrefix+passengerphone;
+        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
     }
 }
