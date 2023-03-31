0c7909ea85e44b0215158def9b658406a0ad4a31
liujin <liujin103827@163.com>
2023-03-31 23:10:13 +0800

    添加了乘客获取验证码

--- api-passenger/src/main/java/com/liujin/apipassenger/controller/VerificationCodeController.java
+++ api-passenger/src/main/java/com/liujin/apipassenger/controller/VerificationCodeController.java
@@ -2,6 +2,7 @@ package com.liujin.apipassenger.controller;
 
 import com.liujin.apipassenger.request.VerificationCodeDTO;
 import com.liujin.apipassenger.service.VerificationCodeService;
+import com.liujin.internalcommon.dto.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +14,8 @@ public class VerificationCodeController {
     @Autowired
     private VerificationCodeService verificationCodeService;
 
-    @GetMapping("verification-Code")
-    public String verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
+    @GetMapping("verification-code")
+    public ResponseResult verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
        String passengerphone =  verificationCodeDTO.getPassengerPhone();
         return verificationCodeService.getPassengerPhone(passengerphone);
     }
