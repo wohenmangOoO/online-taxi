2632211d9a27beaee68aaeaf35beee99da62ec62
liujin <liujin103827@163.com>
2023-03-31 23:10:53 +0800

    调用验证码服务

---
+++ api-passenger/src/main/java/com/liujin/apipassenger/remote/ServiceVerificationCodeClient.java
@@ -0,0 +1,14 @@
+package com.liujin.apipassenger.remote;
+
+import com.liujin.internalcommon.dto.ResponseResult;
+import com.liujin.internalcommon.response.NumberCodeResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@FeignClient("service-verificationcode")
+public interface ServiceVerificationCodeClient {
+
+    @RequestMapping(method = RequestMethod.GET,value = "/numberCode/{size}")
+    ResponseResult<NumberCodeResponse> getNumberCode(@PathVariable("size") int size);
+}
