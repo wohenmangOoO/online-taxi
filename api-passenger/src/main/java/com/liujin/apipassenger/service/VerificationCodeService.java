09a1b24c2321869005751c01e73cc7c69881ff6b
liujin <liujin103827@163.com>
2023-04-08 23:16:12 +0800

    校验redis验证码

--- api-passenger/src/main/java/com/liujin/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/liujin/apipassenger/service/VerificationCodeService.java
@@ -1,12 +1,17 @@
 package com.liujin.apipassenger.service;
 
+import com.liujin.apipassenger.remote.ServicePassengerUserClient;
 import com.liujin.apipassenger.remote.ServiceVerificationCodeClient;
-import com.liujin.apipassenger.request.VerificationCodeDTO;
+import com.liujin.apipassenger.util.JwtUtils;
+import com.liujin.apipassenger.util.RedisPrefixUtils;
 import com.liujin.internalcommon.constant.CommonStatusEnum;
+import com.liujin.internalcommon.constant.IdentityConstants;
+import com.liujin.internalcommon.constant.TokenConstants;
 import com.liujin.internalcommon.dto.ResponseResult;
+import com.liujin.internalcommon.dto.TokenResult;
+import com.liujin.internalcommon.request.VerificationCodeDTO;
 import com.liujin.internalcommon.response.NumberCodeResponse;
-import com.liujin.internalcommon.response.TokenResoinse;
-import net.sf.json.JSONObject;
+import com.liujin.internalcommon.response.TokenResponse;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -21,10 +26,15 @@ public class VerificationCodeService {
     private ServiceVerificationCodeClient serviceVerificationCodeClient;
 
     @Autowired
+    private ServicePassengerUserClient servicePassengerUserClient;
+
+    @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
 
-    private String verificationCodePrefix = "passenger-verification-code-";
+
+
+
 
     //生成验证码
     public ResponseResult getPassengerPhone(String passengerphone ){
@@ -37,33 +47,64 @@ public class VerificationCodeService {
         int numberCode = numberCoderesponse.getData().getNumbercode();
         return numberCode;
     }
-    private String passengerphoneKey(String passengerphone){
-        return verificationCodePrefix+passengerphone;
-    }
+
     private void setRedis(String passengerphone, int numberCode){
-        stringRedisTemplate.opsForValue().set(passengerphoneKey(passengerphone),numberCode+"",2, TimeUnit.MINUTES);
+        String key = RedisPrefixUtils.passengerphoneKey(passengerphone);
+        stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
     }
 
-
+    //校验验证码
     public ResponseResult chvckVerificationCode(VerificationCodeDTO verificationCodeDTO){
         String passengerphone = verificationCodeDTO.getPassengerPhone();
         String verificationCode = verificationCodeDTO.getVerificationCode();
         String value = getValue(passengerphone);
-        return chackCode(value,verificationCode);
+        boolean chackCode = chackCode(value, verificationCode);
+        if (!chackCode){
+            return ResponseResult.fail(CommonStatusEnum.FAIL.getCode(),CommonStatusEnum.FAIL.getValue());
+        }
+        judgmentUserExist(passengerphone);
+
+        TokenResult tokenResult = new TokenResult();
+        tokenResult.setPhone(passengerphone);
+        tokenResult.setIdentity(IdentityConstants.PASSENGER_IDENTITY);
+        tokenResult.setType(TokenConstants.ASSESS_TOKEN_TYPE);
+        String assessToken = generatorToken(tokenResult);
+        tokenDepositRedis(assessToken,tokenResult,30);
+
+        tokenResult.setType(TokenConstants.REFRESH_TOKEN_TYPE);
+        String refreshToken = generatorToken(tokenResult);
+        tokenDepositRedis(refreshToken,tokenResult,31);
+
+        TokenResponse tokenResponse = new TokenResponse();
+        tokenResponse.setAccessToken(assessToken);
+        tokenResponse.setRefreshToken(refreshToken);
+        return ResponseResult.success(tokenResponse);
+    }
+    private String generatorToken(TokenResult tokenResult){
+        String token = JwtUtils.generatorToken(tokenResult.getPhone(),tokenResult.getIdentity() , tokenResult.getType());
+        return token;
+    }
+    private void tokenDepositRedis(String token,TokenResult tokenResult,int timeOut){
+        stringRedisTemplate.opsForValue().set(RedisPrefixUtils.tokenKey(tokenResult), token,timeOut,TimeUnit.DAYS);
     }
     private String getValue(String passengerphone){
-        String key = passengerphoneKey(passengerphone);
+        String key = RedisPrefixUtils.passengerphoneKey(passengerphone);
         return stringRedisTemplate.opsForValue().get(key);
     }
-    private ResponseResult chackCode(String value,String verificationCode){
+    private boolean chackCode(String value,String verificationCode){
         if (StringUtils.isBlank(value)) {
-            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+            return false;
         }
-        if (verificationCode.trim().equals(value)){
-            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        if (!verificationCode.trim().equals(value)){
+            return false;
         }
-        TokenResoinse token = new TokenResoinse();
-        token.setToken("token str");
-        return ResponseResult.success(CommonStatusEnum.VERIFICATION_CODE_SUCCESS.getCode(),CommonStatusEnum.VERIFICATION_CODE_SUCCESS.getValue(), token);
+
+        return true;
     }
+    private void judgmentUserExist(String passengerPhone){
+        VerificationCodeDTO user = new VerificationCodeDTO();
+        user.setPassengerPhone(passengerPhone);
+        servicePassengerUserClient.loginOrRegister(user);
+    }
+
 }
