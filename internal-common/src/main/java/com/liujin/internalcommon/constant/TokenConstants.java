dd81a7d04179adfd4b9d7f99144aaadbe982a8b8
liujin <liujin103827@163.com>
2023-04-08 23:20:36 +0800

    双token常量

---
+++ internal-common/src/main/java/com/liujin/internalcommon/constant/TokenConstants.java
@@ -0,0 +1,7 @@
+package com.liujin.internalcommon.constant;
+
+public class TokenConstants {
+
+    public static final String ASSESS_TOKEN_TYPE = "assessToken";
+    public static final String REFRESH_TOKEN_TYPE = "refreshToken";
+}
