baed0a48fd6d99c14e115dcf768cba2fa7e5fe9a
liujin <liujin103827@163.com>
2023-04-08 23:20:09 +0800

    token身份常量0：用户 1：司机

---
+++ internal-common/src/main/java/com/liujin/internalcommon/constant/IdentityConstants.java
@@ -0,0 +1,7 @@
+package com.liujin.internalcommon.constant;
+
+public class IdentityConstants {
+
+    public static final String PASSENGER_IDENTITY = "0";
+    public static final String DRIVER_IDENTITY = "1";
+}
