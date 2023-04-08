794ab56c5318521f7818f951400c9321907894cd
liujin <liujin103827@163.com>
2023-04-08 23:22:54 +0800

    添加了token实体类

---
+++ internal-common/src/main/java/com/liujin/internalcommon/dto/TokenResult.java
@@ -0,0 +1,13 @@
+package com.liujin.internalcommon.dto;
+
+import lombok.Data;
+
+@Data
+public class TokenResult {
+    private String phone;
+    private String identity;
+    private String type;
+
+
+}
+
